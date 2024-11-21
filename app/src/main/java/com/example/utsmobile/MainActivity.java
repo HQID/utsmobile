001433d6b4eaade35f7347606ab5166fa471121b
HQID <hazuragaming2234@gmail.com>
2024-11-21 16:48:58 +0800

    tes

---
+++ app/src/main/java/com/example/utsmobile/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.utsmobile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize views
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        // Set up adapter and fragments
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentRecommended(), "Recommend");
+        adapter.addFragment(new FragmentRamen(), "Food");
+        adapter.addFragment(new FragmentIceCream(), "Dessert");
+        adapter.addFragment(new FragmentDrink(), "Drink");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.recommendation);
+        tabLayout.getTabAt(1).setIcon(R.drawable.food);
+        tabLayout.getTabAt(2).setIcon(R.drawable.dessert);
+        tabLayout.getTabAt(3).setIcon(R.drawable.drink);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            layoutParams.setMargins(20, 0, 20, 0); // Margin kiri, atas, kanan, bawah
+            tab.requestLayout();
+        }
+
+    }
+}
